a9c608328b5fce2ca6ce302177bf679afb7ca895
arnerim <marco.arneri@tasgroup.eu>
2024-03-25 15:14:12 +0100

    B4BPTAS-117 - wip

--- src/main/java/eu/tasgroup/springbootguide/controller/model/DemoRequest.java
+++ src/main/java/eu/tasgroup/springbootguide/controller/model/DemoRequest.java
@@ -1,6 +1,5 @@
 package eu.tasgroup.springbootguide.controller.model;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -9,16 +8,13 @@ import lombok.Data;
 @Data
 public class DemoRequest {
 
-    @Schema(example = "IUV123456", description = "Identificativo univoco")
     @NotBlank
     @Size(max = 70)
     private String iuv;
 
-    @Schema(example = "MI", description = "Citta")
     @Pattern(regexp = "(?:[A-Z]{2})")
     private String city;
 
-    @Schema(example = "IT", description = "Nazione")
     @Pattern(regexp = "(?:[A-Z]{2})")
     private String nation;
 }
