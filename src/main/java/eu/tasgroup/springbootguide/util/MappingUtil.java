0dc097a22885d8cdecfa93314d2f04199831dc8b
arnerim <marco.arneri@tasgroup.eu>
2024-03-26 15:13:29 +0100

    B4BPTAS-117 - refactor readme

--- src/main/java/eu/tasgroup/springbootguide/util/MappingUtil.java
+++ src/main/java/eu/tasgroup/springbootguide/util/MappingUtil.java
@@ -7,17 +7,11 @@ import static eu.tasgroup.springbootguide.util.AppConstant.*;
 
 public class MappingUtil {
 
-    private MappingUtil() {}
-
-    public static ComponentEnum component() {
-        return ComponentEnum.valueOf(MDC.get(MDC_COMPONENT));
+    private MappingUtil() {
     }
 
     public static String callerId() {
         return MDC.get(CALLER_ID_KEY);
     }
 
-    public static String scheduledId() {
-        return MDC.get(MDC_SCHEDULED_ID);
-    }
 }
